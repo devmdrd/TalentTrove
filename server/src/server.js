7922e01e86b2cda8dc1a31b17b99edcb7b249d1a
MUHAMMED RASHID <98721551+mdrd-official@users.noreply.github.com>
2024-05-10 00:48:33 +0530

    Update server.js

--- server/src/server.js
+++ server/src/server.js
@@ -18,7 +18,7 @@ const app = express();
 
 app.use(
   cors({
-    origin: "http://localhost:3000",
+    origin: "https://talenttrove.live",
     methods: "GET,HEAD,PUT,PATCH,POST,DELETE",
     credentials: true,
   })
@@ -97,7 +97,7 @@ const server = http.createServer(app);
 
 const io = new Server(server, {
   cors: {
-    origin: "http://localhost:3000",
+    origin: "https://talenttrove.live",
     methods: ["GET", "POST"],
     credentials: true,
   },
