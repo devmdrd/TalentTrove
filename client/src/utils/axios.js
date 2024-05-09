423e5e22049606972ceb72fe9c1631bc6252c383
MUHAMMED RASHID <98721551+mdrd-official@users.noreply.github.com>
2024-05-10 00:47:10 +0530

    Update axios.js

--- client/src/utils/axios.js
+++ client/src/utils/axios.js
@@ -1,6 +1,6 @@
 import axios from "axios";
 
-const BASE_URL = "http://localhost:3001/api";
+const BASE_URL = "https://talenttrove-9jlw.onrender.com/api";
 
 const api = axios.create({
   baseURL: BASE_URL,
