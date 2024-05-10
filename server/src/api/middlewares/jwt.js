01020602a0aa2c5a6e16844c1d609e7bc40ed56f
MUHAMMED RASHID <98721551+mdrd-official@users.noreply.github.com>
2024-05-10 14:40:10 +0530

    Update jwt.js

--- server/src/api/middlewares/jwt.js
+++ server/src/api/middlewares/jwt.js
@@ -8,7 +8,6 @@ const Company = require("../models/companyModel");
 const verifyToken = async (req, res, next) => {
   try {
     const token = req.headers.authorization.split(" ")[1];
-    console.log(token);
     if (!token) {
       return res.status(401).json({ error: "Unauthorized" });
     }
