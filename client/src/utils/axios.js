51ef45b0c064fc6b9038091e51fc92da89a30c37
MUHAMMED RASHID <98721551+mdrd-official@users.noreply.github.com>
2024-05-10 08:26:28 +0530

    Update axios.js

--- client/src/utils/axios.js
+++ client/src/utils/axios.js
@@ -12,7 +12,6 @@ const api = axios.create({
 api.interceptors.request.use(
   (config) => {
     const token = localStorage.getItem("token"); 
-    console.log(token)
     if (token) {
       config.headers.Authorization = `Bearer ${token}`;
     }
