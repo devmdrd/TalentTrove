601bf724232dd0133602d7dc4a70e782b8e1ed12
MUHAMMED RASHID <98721551+mdrd-official@users.noreply.github.com>
2024-05-12 10:55:01 +0530

    Update JobsPosted.jsx

--- client/src/pages/recruiter/JobsPosted.jsx
+++ client/src/pages/recruiter/JobsPosted.jsx
@@ -1,14 +1,14 @@
 import React from 'react'
 import Header from '../../components/recruiter/Header'
 import PostedJobs from '../../components/recruiter/PostedJobs'
-// import Footer from '../../components/recruiter/Footer'
+import Footer from '../../components/recruiter/Footer'
 
 const JobsPosted = () => {
   return (
     <>
       <Header />
       <PostedJobs />
-      {/* <Footer /> */}
+      <Footer />
     </>
   )
 }
