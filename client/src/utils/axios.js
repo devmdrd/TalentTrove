04165b4b281bec97401648647a1675b8f1d93ccc
MUHAMMED RASHID <98721551+mdrd-official@users.noreply.github.com>
2024-05-26 11:57:02 +0530

    Update axios.js

--- client/src/utils/axios.js
+++ client/src/utils/axios.js
@@ -2,6 +2,7 @@ import axios from "axios";
 
 const BASE_URL = "https://talenttrove-9jlw.onrender.com/api";
 
+
 const api = axios.create({
   baseURL: BASE_URL,
   withCredentials: true,
@@ -9,24 +10,9 @@ const api = axios.create({
     "Content-Type": "application/json"
   }
 });
-
-const getToken = () => {
-  const localStorageToken = localStorage.getItem("token");
-  if (localStorageToken) {
-    return localStorageToken;
-  }
-
-  const cookieToken = document.cookie
-    .split("; ")
-    .find(row => row.startsWith("token="))
-    ?.split("=")[1];
-
-  return cookieToken || null;
-};
-
 api.interceptors.request.use(
   (config) => {
-    const token = getToken(); 
+    const token = localStorage.getItem("token"); 
     if (token) {
       config.headers.Authorization = `Bearer ${token}`;
     }
@@ -44,12 +30,9 @@ api.interceptors.response.use(
   (error) => {
     if (error.response && error.response.status === 401) {
       localStorage.removeItem("token");
-      document.cookie = 'token=; path=/; expires=Thu, 01 Jan 1970 00:00:00 GMT';
       // window.location.href = "http://localhost:3000/signin";
     }
     return Promise.reject(error);
   }
 );
-
 export default api;
-
